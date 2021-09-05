31f1fd17a9223dc786dd390322f8e95a47ebb9f5
Vinit Rathod <vinitrathod100901@gmail.com>
2021-09-05 21:07:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { SignUp } from './components/SignUp';
+import img from './assests/personImage.jpg'
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '50%',
+    margin: "6rem auto",
+    border: "1px solid #999",
+    "& .MuiStepIcon-root.MuiStepIcon-active": {
+      color: "red",
+    },
+    "& .MuiStepIcon-root.MuiStepIcon-completed":{
+      color:"red",
+    },
+    },
+  backButton: {
+    marginRight: theme.spacing(1),
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+}));
+
+function getSteps() {
+  return ['Sign Up', 'Login', 'Update Details'];
+}
+
+function getStepContent(stepIndex) {
+  switch (stepIndex) {
+    case 0:
+      return (
+      
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-5">
+          <SignUp />
+        </div>
+        <div className="col-md-7">
+          <img className="img-fluid vector-img w-100" src={img} alt="" />
+        </div>
+      </div>
+    </div>
+      );
+    case 1:
+      return (
+        <div>
+          Login Side
+        </div>
+      );
+    case 2:
+      return 'Testing';
+    default:
+      return 'Unknown stepIndex';
+  }
+}
+
+export default function HorizontalLabelPositionBelowStepper() {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = getSteps();
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {activeStep === steps.length ? (
+          <div>
+            <Typography className={classes.instructions}>All steps completed</Typography>
+            <Button onClick={handleReset}>Reset</Button>
+          </div>
+        ) : (
+          <div>
+            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+            <div className='container'>
+              <Button
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                className={classes.backButton}
+              >
+                Back
+              </Button>
+              <Button variant="contained" color="primary" onClick={handleNext}>
+                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;
